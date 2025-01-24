21cc48beeb4e0c6cc1f2b854ebfb99cb0a88d784
Brian Somogyi <brian.somogyi@emanuel.ro>
2025-01-24 21:58:16 +0200

    Used "router" as a package

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,15 +10,6 @@ import HelloWorld from './components/HelloWorld.vue'
     </nav>
   </header>
   <RouterView />
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style>
