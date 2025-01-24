5818d11d2e23b55a7699534d67265d963eece021
briansomogyi <147399516+briansomogyi@users.noreply.github.com>
2025-01-24 20:52:38 +0200

    Added vue-router (#5)

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import About from './components/About.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: "/", component: HelloWorld },
+    { path: "/about", component: About },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount("#app");
