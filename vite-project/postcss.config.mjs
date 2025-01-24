d453284cafc43216fb9a4641e0785d0572cd1bc2
Brian Somogyi <brian.somogyi@emanuel.ro>
2025-01-24 20:32:14 +0200

    Added tailwindcss

---
+++ vite-project/postcss.config.mjs
@@ -0,0 +1,8 @@
+const config = {
+    plugins: {
+        tailwindcss: {},
+        "@tailwindcss/postcss": {},
+        autoprefixer: {},
+    },
+};
+export default config;
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [tailwindcss(), vue()],
+  server: {
+    fs: {
+      allow: [
+        '..'
+      ],
+    },
+  },
 })
