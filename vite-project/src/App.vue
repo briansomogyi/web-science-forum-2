bf7d417fc324c2a2f6030b778d880916fb3cc2e4
briansomogyi <147399516+briansomogyi@users.noreply.github.com>
2025-01-28 15:59:31 +0200

    Created "src/views/AskQuestionView.vue" and updated "src/App.vue" and "src/router/index.js" (#9)

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -8,6 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/questions">Questions</RouterLink>
+      <RouterLink to="/questions/ask">Ask a question</RouterLink>
     </nav>
   </header>
   <RouterView />
