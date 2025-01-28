59a1a3903d32c20ac6dfcb80e2536fc8038e8e01
briansomogyi <brian.somogyi@emanuel.ro>
2025-01-28 15:49:33 +0200

    Created "src/views/QuestionsView.vue" and updated "src/App.vue" and "src/router/index.js"

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -12,6 +12,11 @@ const router = createRouter({
             path: '/about',
             name: 'about',
             component: () => import('../views/AboutView.vue')
+        },
+        {
+            path: '/questions',
+            name: 'questions',
+            component: () => import('../views/QuestionsView.vue')
         }
     ]
 })
