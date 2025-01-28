3779cccb193f7ef43383ebc8b7acc48407a89a0f
briansomogyi <brian.somogyi@emanuel.ro>
2025-01-28 20:29:02 +0200

    Completed the about section

--- vite-project/src/components/About.vue
+++ vite-project/src/components/About.vue
@@ -1,3 +1,16 @@
-<script setup></script>
-<template></template>
-<script></script>
+<template>
+    <div class="info-layer p-4 bg-white shadow-lg rounded-lg">
+        <div class="flex items-center">
+            <i class="bi bi-info-circle text-blue-500 text-2xl mr-2"></i>
+            <h2 class="text-xl font-semibold">Empowering the world to develop technology through collective knowledge
+            </h2>
+        </div>
+        <p class="mt-2 text-gray-600">Our products and tools enable people to ask, share and learn at work or at home.
+        </p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
--- vite-project/src/views/AboutView.vue
+++ vite-project/src/views/AboutView.vue
@@ -1,3 +1,15 @@
-<script setup></script>
-<template></template>
+<script setup>
+import About from '../components/About.vue';
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+</script>
+<template>
+    <div class="flex min-h-screen bg-gray-100">
+        <Sidebar />
+        <div class="flex-1 flex flex-col">
+            <Header />
+            <About />
+        </div>
+    </div>
+</template>
 <style></style>
