1306fd7c12d45192601de8dc188e3d367089b088
briansomogyi <147399516+briansomogyi@users.noreply.github.com>
2025-01-30 02:46:02 +0200

    Added "Help" (#21)

---
+++ vite-project/src/views/HelpCenterView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex min-h-screen bg-gray-100">
+        <Sidebar />
+        <div class="flex-1 flex flex-col">
+            <Header />
+            <HelpCenter />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+import HelpCenter from '../components/HelpCenter.vue';
+</script>
